fb023c60f880d4972fed1d302c676398ca94f6e6
Thiago Figueiredo <1029909+thiagofuer@users.noreply.github.com>
2026-01-21 10:56:20 -0400

    feat: configuração do Spring Security e política de CORS

---
+++ backend-api/src/main/java/br/gov/mt/seplag/backendapi/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package br.gov.mt.seplag.backendapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) //Desabilitando csfr pois usaremos JWT (Stateless)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //Desabilitando a sessão pois não é necessário para JWT
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v1/auth/**").permitAll() //Excluindo o endpoint da autenticação da necessidade de usar token
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() //Libera o Swagger tambem
+                        .anyRequest().authenticated() //Agora o restante das urls exigem token
+                )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())); //Carrega as configurações do CORS
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        //a) Segurança: bloquear acesso ao endpoint a partir de domínios fora do domínio do serviço
+        // Como não foi especificado qual o dominio, usei o localhost e o seplag
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://seplag.mt.gov.br"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
