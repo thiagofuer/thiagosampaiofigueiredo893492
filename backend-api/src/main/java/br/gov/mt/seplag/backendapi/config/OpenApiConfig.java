b399e827e02e905569b05ef72565e176e763820b
Thiago Figueiredo <1029909+thiagofuer@users.noreply.github.com>
2026-01-28 20:56:06 -0400

    config(swagger): habilita autenticação JWT (Bearer Token) no Swagger UI

--- backend-api/src/main/java/br/gov/mt/seplag/backendapi/config/OpenApiConfig.java
+++ backend-api/src/main/java/br/gov/mt/seplag/backendapi/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package br.gov.mt.seplag.backendapi.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,19 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .info(new Info()
                         .title("API de Gestão de Artistas e Álbuns - SEPLAG-MT")
                         .version("v1")
-                        .description("Serviço desenvolvido para o Processo Seletivo Simplificado (PSS) da SEPLAG-MT."));
+                        .description("Serviço desenvolvido para o Processo Seletivo Simplificado (PSS) da SEPLAG-MT."))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
